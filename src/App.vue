57e2bc1f12517c8c833b22a60e3b9e2984d5ea93
JB Bal <bal@abvent.fr>
2024-04-09 15:15:01 +0200

    🚧 First version of the button grid

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
     <img src="/logo/logo-long.svg" class="app-logo app-logo--long" />
     <img src="/logo/logo-square.svg" class="app-logo app-logo--square" />
   </header>
+  <div class="button-grid">
+  </div>
 </template>
 
 <style lang="scss">
@@ -19,6 +21,11 @@
 }
 
 body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
   align-items: center;
   background: center / cover no-repeat url('/background.jpg'), #ABDDFF;
   box-sizing: border-box;
@@ -57,6 +64,35 @@ body {
     @include small-screen {
       display: block;
     }
+  }
+}
+
+.button-grid {
+  -webkit-backdrop-filter: blur(28px);
+  backdrop-filter: blur(28px);
+  background-color: rgba(255,255,255,.4);
+  border: 1px solid rgba(255,255,255,.4);
+  border-radius: 50px;
+  box-sizing: border-box;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 36px 64px;
+  max-width: 1080px;
+  padding: 56px 64px;
+  width: 100%;
+
+  height: 500px;
+
+  @include medium-screen {
+    border-radius: 42px;
+    gap: 24px 48px;
+    padding: 40px 48px;
+  }
+
+  @include small-screen {
+    border-radius: 26px;
+    gap: 20px;
+    padding: 24px;
   }
 }
 
