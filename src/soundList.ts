935de6e56a2b0689439612ae0e21a301e6680fb4
JB Bal <bal@abvent.fr>
2024-04-09 17:39:26 +0200

    🔖 V1 🎉

--- src/soundList.ts
+++ src/soundList.ts
@@ -34,5 +34,110 @@ export const soundList: Sound[] = [
     url: 'fion',
     label: 'Ca me fait penser a...',
     color: 'pink'
+  },
+  {
+    url: 'fromage',
+    label: 'La petite lingette',
+    color: 'blue'
+  },
+  {
+    url: 'gratte',
+    label: 'Le bout d\'la trompe',
+    color: 'yellow'
+  },
+  {
+    url: 'gros',
+    label: 'C\'est rigolo',
+    color: 'brown'
+  },
+  {
+    url: 'lili',
+    label: 'Si on trouve pas...',
+    color: 'pink'
+  },
+  {
+    url: 'magique',
+    label: 'Dans le bois magique',
+    color: 'yellow'
+  },
+  {
+    url: 'morue',
+    label: 'Filet de morue',
+    color: 'blue'
+  },
+  {
+    url: 'mou',
+    label: 'Tout mou',
+    color: 'pink'
+  },
+  {
+    url: 'noel',
+    label: 'Les boules de papa',
+    color: 'brown'
+  },
+  {
+    url: 'non',
+    label: 'Non',
+    color: 'blue'
+  },
+  {
+    url: 'orgasme',
+    label: 'Ah ouuuuuiiii heeeuuuu',
+    color: 'yellow'
+  },
+  {
+    url: 'poemes',
+    label: 'Ecrire des poemes',
+    color: 'pink'
+  },
+  {
+    url: 'proustate',
+    label: 'C\'est quoi ?',
+    color: 'blue'
+  },
+  {
+    url: 'regles',
+    label: 'C\'est parce que...',
+    color: 'yellow'
+  },
+  {
+    url: 'ruminant',
+    label: 'T\'es un p\'tit cochon',
+    color: 'brown'
+  },
+  {
+    url: 'slip',
+    label: 'Eclater mon slip',
+    color: 'blue'
+  },
+  {
+    url: 'temps',
+    label: 'Un peu de temps',
+    color: 'pink'
+  },
+  {
+    url: 'univers',
+    label: 'Destruction totale',
+    color: 'yellow'
+  },
+  {
+    url: 'vegan',
+    label: 'Connard de vegan',
+    color: 'blue'
+  },
+  {
+    url: 'vibration',
+    label: 'De la vibration',
+    color: 'brown'
+  },
+  {
+    url: 'violence',
+    label: 'Spirale de violence',
+    color: 'yellow'
+  },
+  {
+    url: 'zizi',
+    label: 'Mais docteur !',
+    color: 'pink'
   }
 ]
