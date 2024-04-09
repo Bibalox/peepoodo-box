ca0cb3bd00c9f7e542bb3385298bffa041d07850
JB Bal <bal@abvent.fr>
2024-04-09 16:35:46 +0200

    💄 ♻️ Improve the main layout

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,17 @@ import { soundList } from '@assets/soundList.ts'
 </script>
 
 <template>
-  <header class="app-header">
-    <img src="/logo/logo-long.svg" class="app-logo app-logo--long" />
-    <img src="/logo/logo-square.svg" class="app-logo app-logo--square" />
+  <header class="app__header">
+    <img src="/logo/logo-long.svg" class="app__logo app__logo--long" />
+    <img src="/logo/logo-square.svg" class="app__logo app__logo--square" />
   </header>
-  <div class="button-grid">
+  <main class="app__main">
     <sound-button
       v-for="sound in soundList" :key="sound.url"
       :url="sound.url" :label="sound.label"
       :color="sound.color"
     />
-  </div>
+  </main>
 </template>
 
 <style lang="scss">
@@ -28,21 +28,22 @@ import { soundList } from '@assets/soundList.ts'
 }
 
 body {
+  background: center / cover no-repeat url('/background.jpg'), #ABDDFF;
   margin: 0;
   padding: 0;
+  overflow: hidden;
+  height: 100dvh;
 }
 
-#app {
+.app {
   align-items: center;
-  background: center / cover no-repeat url('/background.jpg'), #ABDDFF;
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
   gap: 56px;
-  margin: 0;
-  min-height: 100vh;
+  height: 100%;
+  overflow-y: auto;
   padding: 64px 48px 56px;
-  width: 100%;
 
   @include medium-screen {
     gap: 48px;
@@ -52,54 +53,52 @@ body {
   @include small-screen {
     padding: 48px 16px 16px;
   }
-}
 
-.app-logo {
-  flex-shrink: 0;
+  &__logo {
+    flex-shrink: 0;
 
-  &--long {
-    display: block;
-
-    @include small-screen {
-      display: none;
-    }
-  }
-
-  &--square {
-    display: none;
-
-    @include small-screen {
+    &--long {
       display: block;
+
+      @include small-screen {
+        display: none;
+      }
+    }
+
+    &--square {
+      display: none;
+
+      @include small-screen {
+        display: block;
+      }
     }
   }
-}
 
-.button-grid {
-  -webkit-backdrop-filter: blur(28px);
-  backdrop-filter: blur(28px);
-  background-color: rgba(255,255,255,.4);
-  border: 1px solid rgba(255,255,255,.4);
-  border-radius: 50px;
-  box-sizing: border-box;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 36px 64px;
-  max-width: 1080px;
-  padding: 56px 64px;
-  width: 100%;
+  &__main {
+    -webkit-backdrop-filter: blur(28px);
+    backdrop-filter: blur(28px);
+    background-color: rgba(255,255,255,.4);
+    border: 1px solid rgba(255,255,255,.4);
+    border-radius: 50px;
+    box-sizing: border-box;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 36px 64px;
+    max-width: 1080px;
+    padding: 56px 64px;
+    width: 100%;
 
-  height: 500px;
+    @include medium-screen {
+      border-radius: 42px;
+      gap: 24px 48px;
+      padding: 40px 48px;
+    }
 
-  @include medium-screen {
-    border-radius: 42px;
-    gap: 24px 48px;
-    padding: 40px 48px;
-  }
-
-  @include small-screen {
-    border-radius: 26px;
-    gap: 20px;
-    padding: 24px;
+    @include small-screen {
+      border-radius: 26px;
+      gap: 20px;
+      padding: 24px;
+    }
   }
 }
 
