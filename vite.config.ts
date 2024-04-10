1db00b57d701387a61c0938d874a2d4094483fa6
JB Bal <bal@abvent.fr>
2024-04-10 10:57:01 +0200

    🚧 ✨ First draft for the PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: [
+        '/app-icons/favicon.png',
+        '/app-icons/apple-touch-icon.png',
+        '/app-icons/safari-pinned-tab.svg'
+      ],
+      manifest: {
+        name: 'Peepoodo box',
+        short_name: 'Peepoodo box',
+        description: 'Mais docteur, vous avez un zizi ?',
+        display: 'fullscreen',
+        theme_color: '#ABDDFF',
+        icons: [
+          {
+            src: '/app-icons/android-chrome-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/app-icons/android-chrome-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ]
 })
 
